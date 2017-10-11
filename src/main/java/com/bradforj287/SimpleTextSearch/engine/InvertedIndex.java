a52dc35c4b8921d5fcfd3a51367a1c6cb3efa0c7
lytles@takashi <srl.source@nqzero.com>
2017-10-10 23:14:43 -0400

    still working - cleanup inverted index

--- src/main/java/com/bradforj287/SimpleTextSearch/engine/InvertedIndex.java
+++ src/main/java/com/bradforj287/SimpleTextSearch/engine/InvertedIndex.java
@@ -1,6 +1,5 @@
 package com.bradforj287.SimpleTextSearch.engine;
 
-import com.bradforj287.SimpleTextSearch.*;
 import com.google.common.collect.ImmutableMap;
 
 import java.util.*;
@@ -9,47 +8,39 @@ import java.util.*;
  * Created by brad on 6/6/15.
  */
 public class InvertedIndex {
-    private ImmutableMap<String,HashSet<ParsedDocument>> termToPostings;
+    private HashMap<String,HashSet<ParsedDocument>> index = new HashMap<>();
     private DocumentParser searchTermParser = new DocumentParser(false, false);
 
     public InvertedIndex(List<ParsedDocument> corpus) {
-        // build term -> posting map
-        Map<String,HashSet<ParsedDocument>> map = new HashMap<>();
+        // build the reverse index, ie from word to list of documents
         for (ParsedDocument doc : corpus) {
             for (String word : doc.getUniqueWords()) {
-                if (!map.containsKey(word)) map.put(word, new HashSet());
-                map.get(word).add(doc);
+                if (!index.containsKey(word)) index.put(word, new HashSet());
+                index.get(word).add(doc);
             }
         }
-        termToPostings = ImmutableMap.copyOf(map);
     }
 
 
 
-    private ArrayList<Set<ParsedDocument>> getRelevantDocuments(ParsedDocument searchDoc) {
-        ArrayList<Set<ParsedDocument>> retVal = new ArrayList<>();
-        for (String word : searchDoc.getUniqueWords())
-            if (termToPostings.containsKey(word))
-                retVal.add(termToPostings.get(word));
-        return retVal;
-    }
 
     public ArrayList<Set<ParsedDocument>> search(String searchTerm) {
-        ParsedDocument searchDocument = searchTermParser.parse(searchTerm,0);
-        ArrayList<Set<ParsedDocument>> documentsToScanSet = getRelevantDocuments(searchDocument);
-        return documentsToScanSet;
+        ParsedDocument searchDoc = searchTermParser.parse(searchTerm,0);
+        ArrayList<Set<ParsedDocument>> docs = new ArrayList<>();
+        for (String word : searchDoc.getUniqueWords())
+            if (index.containsKey(word))
+                docs.add(index.get(word));
+        return docs;
     }
-    public static InvertedIndex buildIndex(ArrayList<String> docs) {
 
+    public static InvertedIndex buildIndex(ArrayList<String> docs) {
         DocumentParser parser = new DocumentParser(true,true);
 
         ArrayList<ParsedDocument> corpus = new ArrayList<>();
         for (int ii=0; ii < docs.size(); ii++)
             corpus.add(parser.parse(docs.get(ii),ii));
         
-        InvertedIndex invertedIndex = new InvertedIndex(corpus);
-
-        return invertedIndex;
+        return new InvertedIndex(corpus);
     }
 
 }
