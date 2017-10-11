4372710f54d72dc5186503963ebcb285fc271d49
lytles@takashi <srl.source@nqzero.com>
2017-10-10 22:38:07 -0400

    still working - parsed doc with naked hashset

--- src/main/java/com/bradforj287/SimpleTextSearch/engine/ParsedDocument.java
+++ src/main/java/com/bradforj287/SimpleTextSearch/engine/ParsedDocument.java
@@ -1,7 +1,6 @@
 package com.bradforj287.SimpleTextSearch.engine;
 
 import com.google.common.base.Preconditions;
-import com.google.common.collect.ImmutableSet;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -12,7 +11,7 @@ import java.util.Set;
  * Created by brad on 6/6/15.
  */
 public class ParsedDocument {
-    private ImmutableSet<String> uniqueWords;
+    private HashSet<String> uniqueWords;
     private Integer uniqueId;
 
     public ParsedDocument(List<DocumentTerm> documentTerms,Integer uniqueId) {
@@ -20,20 +19,17 @@ public class ParsedDocument {
         Preconditions.checkNotNull(documentTerms);
         this.uniqueId = uniqueId;
         HashMap<String, Integer> wordFrequency = new HashMap<>();
-        uniqueWords = null;
 
         for (DocumentTerm t : documentTerms) {
             String word = t.getWord();
-            if (!wordFrequency.containsKey(word)) {
+            if (!wordFrequency.containsKey(word))
                 wordFrequency.put(word, 0);
-
-            }
 
             int count = wordFrequency.get(word);
             wordFrequency.put(word, count + 1);
         }
 
-        uniqueWords = ImmutableSet.copyOf(getUniqueWordsHashSet(documentTerms));
+        uniqueWords = getUniqueWordsHashSet(documentTerms);
     }
 
 
