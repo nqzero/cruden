11fa19500b88e20172e30294248d42e2d6e7db87
lytles@takashi <srl.source@nqzero.com>
2017-10-13 19:30:39 -0400

    tokenizer - move to InvertedIndex

--- src/main/java/com/nqzero/SimpleTextSearch/InvertedIndex.java
+++ src/main/java/com/nqzero/SimpleTextSearch/InvertedIndex.java
@@ -1,7 +1,13 @@
 package com.nqzero.SimpleTextSearch;
 
 
+import java.io.IOException;
 import java.util.*;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.en.KStemFilter;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 
 /**
  * Created by brad on 6/6/15.
@@ -15,7 +21,8 @@ public class InvertedIndex {
     static private ArrayList<Integer> dummy = new ArrayList<>();
     static private boolean usemax = true;
     public boolean printmax = false;
-    TextParseUtils tpu = new TextParseUtils();
+    Analyzer analyzer = new StandardAnalyzer();
+
     
 
     public InvertedIndex() {}
@@ -48,9 +55,11 @@ public class InvertedIndex {
             }
         }
     }
+
     public String stats() {
         return String.format("stats: %4d, %4d, %4d, %4d",numdocs,numtotal,numdummy,nummax);
     }
+
     public ArrayList<Integer> join(ArrayList<Integer> ... lists) {
         ArrayList<Integer> valid = new ArrayList();
         for (int ii=0; ii < lists.length; ii++)
@@ -77,6 +86,7 @@ public class InvertedIndex {
         }
         return result;
     }
+
     static class Ibox {
         public int val;
         public Ibox() {};
@@ -96,7 +106,7 @@ public class InvertedIndex {
         if (text==null || text.isEmpty())
             return new ArrayList<>();
 
-        List<String> terms = tpu.tokenize(text);
+        List<String> terms = tokenize(text);
 
         ArrayList<String> unique = new ArrayList<>();
         HashSet<String> unseen = new HashSet();
@@ -106,10 +116,29 @@ public class InvertedIndex {
 
         return unique;
     }
+
+    ArrayList<String> tokenize(String doc) {
+        ArrayList<String> words = new ArrayList();
+        try (TokenStream raw = analyzer.tokenStream("myfield",doc)) {
+            KStemFilter ts = new KStemFilter(raw);
+            CharTermAttribute term = ts.getAttribute(CharTermAttribute.class);
+            ts.reset();
+            while (ts.incrementToken())
+                // fixme - strip and split at non-word chars
+                words.add(term.toString());
+            ts.end();
+        }
+        catch (IOException ex) {}
+        return words;
+    }
+
     public static void main(String[] args) {
         String doc = "don't hello world the quick brown. fox jumped/over the lazy-dog. light lighter warm warmer dark darker";
         ArrayList<String> list = new InvertedIndex(new ArrayList()).parse(doc);
         for (String word : list)
             System.out.println(word);
+        String adj = "warm warmer light lighter ";
+        for (String word : new InvertedIndex().tokenize(adj+doc))
+                System.out.println(word);
     }
 }
